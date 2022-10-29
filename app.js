11260505cb1c623eb3b773cab0d0a6f79aa88c70
Teemu <teemu9598@gmail.com>
2022-10-30 00:35:02 +0300

    Finish 4.3

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
 
-const mongoUrl = config.MONGODB_URI
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl)
     .then(result => {
-        logger.info('connected to MongoDB')
+        logger.info('connected to MongoDB');
     })
     .catch(error => {
-        logger.info('error connecting to MongoDB:', error.message)
-    })
+        logger.info('error connecting to MongoDB:', error.message);
+    });
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter);
 
-module.exports = app
+module.exports = app;
