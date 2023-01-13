afe9dbb49f1a122bd6bb0e3c33723b3c377f34e3
Bob <teemu9598@gmail.com>
2023-01-13 23:35:40 +0200

    Finish 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,17 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
+const jwt = require('jsonwebtoken');
+
+const extractToken = request => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    };
+
+    return null;
+};
+
 //GET
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1});
@@ -12,10 +23,17 @@ blogsRouter.get('/', async (request, response) => {
 //POST
 blogsRouter.post('/', async (request, response) => {
     const body = request.body;
-    //Select random user
-    const user = await User.findOne();
+    //Get token from request and decode it
+    const token = extractToken(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+
+    //Check validity of token
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+      }
+      const user = await User.findById(decodedToken.id);
     
-    //Create new mongoose Blog object and add random user_id as its owner
+    //Create new mongoose Blog object and add user as its owner
     const blog = new Blog({
         title: body.title,
         author: body.author,
