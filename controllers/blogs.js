278f27ab5a22f57e7328bfab48e279d86e97d82f
Bob <teemu9598@gmail.com>
2023-01-18 21:58:57 +0200

    Finish 4.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,21 +2,6 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const jwt = require('jsonwebtoken');
-
-
-const decodeAndVerifyToken = (token) => {
-    //Decode token, returns null if not valid
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-
-    //Check validity of token
-    if (!token || !decodedToken.id) {
-        return null
-      }
-
-    return decodedToken;
-}
-
 
 //GET
 blogsRouter.get('/', async (request, response) => {
@@ -28,15 +13,12 @@ blogsRouter.get('/', async (request, response) => {
 //POST
 blogsRouter.post('/', async (request, response) => {
     const body = request.body;
-    //Get token from request and decode it
-    const token = request.token
-    const decodedToken = decodeAndVerifyToken(token);
 
-    if (decodedToken === null) {
+    if (request.userId === null) {
         return response.status(401).json({ error: 'token missing or invalid' });
       }
 
-    const user = await User.findById(decodedToken.id);
+    const user = await User.findById(request.userId);
     
     //Create new mongoose Blog object and add user as its owner
     const blog = new Blog({
@@ -76,19 +58,20 @@ blogsRouter.put('/:id', async (request, response) => {
 //DELETE
 blogsRouter.delete('/:id', async (request, response) => {
     const blogId = request.params.id;
-    const token = request.token;
-    const decodedToken = decodeAndVerifyToken(token);
+    const user = request.userId;
+
 
     //Find blog from db
     const blog = await Blog.findById(blogId);
 
+
     //Check if blog has no owner (blog.user=undefined) or the user making the request is not the owner of the blog
-    if (blog.user === undefined || blog.user.toString() !== decodedToken.id) {
+    if (blog.user === undefined || user === null || blog.user.toString() !== user) {
         response.status(401).json({error: 'action denied'});
       }
     
     //if all good, find blog and remove from database
-    else if (blog.user.toString() === decodedToken.id.toString()) {
+    else if (blog.user.toString() === user.toString()) {
         await Blog.findByIdAndRemove(blogId);
         response.status(204).end();
     };
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,12 +1,27 @@
-const logger = require('./logger')
+const logger = require('./logger');
+
+const jwt = require('jsonwebtoken');
+
+const decodeAndVerifyToken = (token) => {
+  //Decode token, returns null if not valid
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  //Check validity of token
+  if (!token || !decodedToken.id) {
+      return null
+    };
+
+  return decodedToken;
+};
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info('Method:', request.method);
+  logger.info('Path:  ', request.path);
+  logger.info('Body:  ', request.body);
+  logger.info('---');
+  next();
+};
+
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization');
@@ -14,16 +29,24 @@ const tokenExtractor = (request, response, next) => {
     request.token = authorization.substring(7)
   }
   else(request.token = null);
+  next();
+};
 
-  next()
-}
+const userExtractor = (request, response, next) => {
+  if (request.token) {
+    request.token = decodeAndVerifyToken(request.token);
+    request.userId = request.token.id;
+  };
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
+  logger.error(error.message);
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
@@ -32,15 +55,16 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({
       error: 'invalid token'
-    })
-  }
+    });
+  };
 
-  next(error)
+  next(error);
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
