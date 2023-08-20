a16bfdae2539b9628f2c7c092bc7d39a953970c5
Bob <teemu9598@gmail.com>
2023-08-20 22:17:01 +0300

    Add reset route for cypress testing

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const middleware = require('./utils/middleware');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl)
-    .then(result => {
-        logger.info('connected to MongoDB');
-    })
-    .catch(error => {
-        logger.info('error connecting to MongoDB:', error.message);
-    });
+.then(result => {
+    logger.info('connected to MongoDB');
+})
+.catch(error => {
+    logger.info('error connecting to MongoDB:', error.message);
+});
 
 app.use(cors());
 app.use(express.json());
@@ -29,6 +29,10 @@ app.use('/api/blogs', middleware.userExtractor, blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/test')
+    app.use('/api/test', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
